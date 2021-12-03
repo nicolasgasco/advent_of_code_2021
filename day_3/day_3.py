e5c50c6fda61b43a646cfd6bd66521a87d92b84b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2021-12-03 19:10:56 +0100

    Second day done

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,15 +1,16 @@
 # First part
 
 def list_from_file(file):
-	"""Create a list from a file"""
-	bins_list = []
-	with open(file) as f:
-		bins = f.readlines()
+    """Create a list from a file"""
+    bins_list = []
+    with open(file) as f:
+        bins = f.readlines()
 
-	for bin in bins:
-		bins_list.append(bin.strip())
-	f.close()
-	return bins_list
+    for bin in bins:
+        bins_list.append(bin.strip())
+    f.close()
+    return bins_list
+
 
 def bin_to_dec(string):
     """Convert from string to 0s and 1s to decimal number"""
@@ -19,6 +20,7 @@ def bin_to_dec(string):
         result += int(string[-i]) * mult
         mult = 2**(i)
     return result
+
 
 gamma = ""
 epsilon = ""
@@ -37,6 +39,33 @@ for i in range(len(bins[0])):
         epsilon += "1"
 
 
-output = open("day_3_output_part_2", "w")
+output = open("day_3_output_part_1", "w")
 output.write(str(bin_to_dec(gamma) * bin_to_dec(epsilon)))
+output.close()
+
+# Part 2
+
+# Find oxigen
+def filter_list(ref, most, least):
+    """Filter list till you have only one element"""
+    lst = ref.copy()
+    while (len(lst) != 1):
+        for i in range(len(lst[0])):
+            sum = 0
+            for bin in lst:
+                sum += int(bin[i])
+            if sum >= (len(lst) / 2):
+                bit_criteria = most
+            else:
+                bit_criteria = least
+            lst = [bin for bin in lst if bin[i] == bit_criteria]
+            if len(lst) == 1:
+                return bin_to_dec(lst[0])
+
+
+oxygen = filter_list(bins.copy(), "1", "0")
+co2 = filter_list(bins, '0', '1')
+
+output = open("day_3_output_part_2", "w")
+output.write(str(co2 * oxygen))
 output.close()
